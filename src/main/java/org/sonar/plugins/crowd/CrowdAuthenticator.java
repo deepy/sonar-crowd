451e5dfc79621cff74f5be943d0873888f6e7829
Evgeny Mandrikov <mandrikov@gmail.com>
2010-01-02 17:52:52 +0000

    Crowd: minor fixes

--- src/main/java/org/sonar/plugins/crowd/CrowdAuthenticator.java
+++ src/main/java/org/sonar/plugins/crowd/CrowdAuthenticator.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.sonar.plugins.crowd;
 
 import com.atlassian.crowd.integration.authentication.PasswordCredential;
@@ -8,6 +21,9 @@ import com.atlassian.crowd.integration.exception.InvalidAuthenticationException;
 import com.atlassian.crowd.integration.exception.InvalidAuthorizationTokenException;
 import com.atlassian.crowd.integration.service.AuthenticationManager;
 import com.atlassian.crowd.integration.service.cache.CachingManagerFactory;
+import com.atlassian.crowd.integration.service.soap.client.ClientProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sonar.api.security.LoginPasswordAuthenticator;
 
 import java.rmi.RemoteException;
@@ -16,13 +32,10 @@ import java.rmi.RemoteException;
  * @author Evgeny Mandrikov
  */
 public class CrowdAuthenticator implements LoginPasswordAuthenticator {
-    @Override
     public void init() {
     }
 
-    @Override
     public boolean authenticate(String login, String password) {
-        // FIXME sout
         try {
             AuthenticationManager authenticationManager = CachingManagerFactory.getAuthenticationManagerInstance();
 
@@ -33,16 +46,20 @@ public class CrowdAuthenticator implements LoginPasswordAuthenticator {
             authenticationManager.authenticate(authenticationContext);
             return true;
         } catch (InvalidAuthenticationException e) {
-            System.out.println("Could not authenticate " + login + ". The username or password were incorrect.");
+            getLog().warn("Could not authenticate " + login + ". The username or password were incorrect.");
         } catch (InactiveAccountException e) {
-            System.out.println("Could not authenticate " + login + ". The account is inactive and the user is not allowed to login.");
+            getLog().warn("Could not authenticate " + login + ". The account is inactive and the user is not allowed to login.");
         } catch (InvalidAuthorizationTokenException e) {
             throw new RuntimeException(e);
         } catch (RemoteException e) {
             throw new RuntimeException(e);
         } catch (ApplicationAccessDeniedException e) {
-            System.out.println("Could not authenticate " + login + ". The user does not have access to authenticate with the Crowd application.");
+            getLog().warn("Could not authenticate " + login + ". The user does not have access to authenticate with the Crowd application.");
         }
         return false;
+    }
+
+    private Logger getLog() {
+        return LoggerFactory.getLogger("org.sonar.plugins.crowd");
     }
 }
--- src/main/java/org/sonar/plugins/crowd/CrowdPlugin.java
+++ src/main/java/org/sonar/plugins/crowd/CrowdPlugin.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.sonar.plugins.crowd;
 
 import org.sonar.api.Extension;
@@ -11,22 +24,18 @@ import java.util.List;
  */
 @SuppressWarnings({"UnusedDeclaration"})
 public class CrowdPlugin implements Plugin {
-    @Override
     public String getKey() {
         return "crowd";
     }
 
-    @Override
     public String getName() {
         return "Crowd";
     }
 
-    @Override
     public String getDescription() {
         return "Plugs authentication mechanism to a Crowd to delegate passwords management.";
     }
 
-    @Override
     public List<Class<? extends Extension>> getExtensions() {
         ArrayList<Class<? extends Extension>> extensions = new ArrayList<Class<? extends Extension>>();
         extensions.add(CrowdAuthenticator.class);
