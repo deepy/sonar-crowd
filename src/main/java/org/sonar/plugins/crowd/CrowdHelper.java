f277774a24e095ab5a001a4e7f1c199304fe41ff
Evgeny Mandrikov <mandrikov@gmail.com>
2010-01-05 09:20:31 +0000

    crowd: upgrade to parent 4

--- src/main/java/org/sonar/plugins/crowd/CrowdHelper.java
+++ src/main/java/org/sonar/plugins/crowd/CrowdHelper.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (C) 2009 Evgeny Mandrikov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.sonar.plugins.crowd;
 
 import org.slf4j.Logger;
