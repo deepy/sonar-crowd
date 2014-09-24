c672d10cf8997f0463573bc40c1adee457c9011f
Julien HENRY <julien.henry@sonarsource.com>
2014-09-24 16:35:56 +0200

    Don't prevent SQ server to start when plugin is installed but not configured

--- src/main/java/org/sonar/plugins/crowd/CrowdRealm.java
+++ src/main/java/org/sonar/plugins/crowd/CrowdRealm.java
@@ -44,16 +44,15 @@ public class CrowdRealm extends SecurityRealm {
 
   private static final Logger LOG = LoggerFactory.getLogger(CrowdRealm.class);
 
-  private final CrowdClient crowdClient;
-  private final CrowdAuthenticator authenticator;
-  private final CrowdUsersProvider usersProvider;
-  private final CrowdGroupsProvider groupsProvider;
+  private final CrowdConfiguration crowdConfiguration;
+
+  private CrowdClient crowdClient;
+  private CrowdAuthenticator authenticator;
+  private CrowdUsersProvider usersProvider;
+  private CrowdGroupsProvider groupsProvider;
 
   public CrowdRealm(CrowdConfiguration crowdConfiguration) {
-    this.crowdClient = createCrowdClient(crowdConfiguration);
-    this.authenticator = new CrowdAuthenticator(crowdClient);
-    this.usersProvider = new CrowdUsersProvider(crowdClient);
-    this.groupsProvider = new CrowdGroupsProvider(crowdClient);
+    this.crowdConfiguration = crowdConfiguration;
   }
 
   private CrowdClient createCrowdClient(CrowdConfiguration configuration) {
@@ -63,16 +62,17 @@ public class CrowdRealm extends SecurityRealm {
     // The password that the application will use when authenticating with the Crowd server.
     crowdProperties.setProperty("application.password", configuration.getCrowdApplicationPassword());
     // Crowd will redirect the user to this URL if their authentication token expires or is invalid due to security restrictions.
-    //crowdProperties.setProperty("application.login.url", "");
+    // crowdProperties.setProperty("application.login.url", "");
     // The URL to use when connecting with the integration libraries to communicate with the Crowd server.
-    //crowdProperties.setProperty("crowd.server.url", "");
+    // crowdProperties.setProperty("crowd.server.url", "");
     // The URL used by Crowd to create the full URL to be sent to users that reset their passwords.
     crowdProperties.setProperty("crowd.base.url", configuration.getCrowdUrl());
     // The session key to use when storing a Boolean value indicating whether the user is authenticated or not.
     crowdProperties.setProperty("session.isauthenticated", "session.isauthenticated");
     // The session key to use when storing a String value of the user's authentication token.
     crowdProperties.setProperty("session.tokenkey", "session.tokenkey");
-    // The number of minutes to cache authentication validation in the session. If this value is set to 0, each HTTP request will be authenticated with the Crowd server.
+    // The number of minutes to cache authentication validation in the session. If this value is set to 0, each HTTP request will be
+    // authenticated with the Crowd server.
     crowdProperties.setProperty("session.validationinterval", "1");
     // The session key to use when storing a Date value of the user's last authentication.
     crowdProperties.setProperty("session.lastvalidation", "session.lastvalidation");
@@ -89,6 +89,10 @@ public class CrowdRealm extends SecurityRealm {
 
   @Override
   public void init() {
+    this.crowdClient = createCrowdClient(crowdConfiguration);
+    this.authenticator = new CrowdAuthenticator(crowdClient);
+    this.usersProvider = new CrowdUsersProvider(crowdClient);
+    this.groupsProvider = new CrowdGroupsProvider(crowdClient);
     try {
       crowdClient.testConnection();
       LOG.info("Crowd configuration is valid, connection test successful.");
